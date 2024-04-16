9abdd877a02cda6f102e82707f4083e25281728d
Trisha-S-10 <162022562+Trisha-S-10@users.noreply.github.com>
2024-04-16 21:29:00 +0530

    Add files via upload

---
+++ Sudoku/src/player/Player.java
@@ -0,0 +1,14 @@
+package player;
+
+public class Player {
+    String name;
+   
+    public Player (String name) {
+        this.name = name;
+    
+    }
+
+    public String getPlayerName() {
+        return this.name;
+    }
+}
